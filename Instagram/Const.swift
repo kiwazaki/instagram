5dde69ac6a8ecd266a4ca255272a5a194efc7a8c
kumiko.iwa <kumiko.hanausagi21@gmail.com>
2017-03-08 18:32:45 +0900

    Initial Commit

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Iwazaki on 2017/03/06.
+//  Copyright © 2017年 Iwazaki. All rights reserved.
+//
+
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  
+//
+//  Created by Iwazaki on 2017/03/06.
+//
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
